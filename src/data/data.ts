28e5a656e9edb3a0c582786bcd74c5ca004c2c51
William To <williamto222@gmail.com>
2024-05-02 20:53:48 +1000

    Added VideoPlayer. Completed Research Page

--- src/data/data.ts
+++ src/data/data.ts
@@ -58,12 +58,18 @@ export const data = {
           {
             name: 'Home',
             href: '/',
-            isExternal: false,
+          },
+          {
+            name: 'Projects',
+            href: '/projects',
+          },
+          {
+            name: 'Research',
+            href: '/research',
           },
           {
             name: 'Contact',
             href: '/contact',
-            isExternal: false,
           },
         ],
       },
@@ -183,6 +189,61 @@ export const data = {
       isFeatured: true,
       thumbnail: '/assets/enigma-run.png',
       githubUrl: 'https://github.com/willyyto/EnigmaRun',
+    },
+  ],
+  research: [
+    {
+      title: 'Comparative Analysis on AI Language Models: ChatGPT-3 and Bard in Debate',
+      imageUrl: '/assets/chatgpt-bard.webp',
+      buttonUrl: 'https://www.williamto.com/assets/files/face_mask_detection_research_question.pdf',
+      description: [
+        "This research paper explores the capabilities and limitations of advanced language models, specifically OpenAI's ChatGPT-3 and Google's Bard, in the context of debating controversial topics. Through structured debates spanning four categories—factual, optional, planning, and problem-solving—this study evaluates each model's performance in terms of relevance, coherence, and factual accuracy. The findings reveal that while both models exhibit proficient linguistic abilities, ChatGPT-3 generally outperforms Bard, especially in maintaining conversational fluidity and mimicking human-like responses. This comparative analysis highlights the evolving efficacy of language models in handling complex communicative tasks and underscores the ongoing need for advancements to address current limitations.",
+      ],
+      stats: [
+        {
+          name: 'Total Debate Categories',
+          value: '4',
+        },
+        {
+          name: 'Total Debates Conducted',
+          value: '20',
+        },
+        {
+          name: 'Language Models Analyzed',
+          value: '2',
+        },
+        {
+          name: 'Key Performance Metrics',
+          value: 'Relevance\n Coherence\n Factual Accuracy',
+        },
+      ],
+    },
+    {
+      title:
+        'Face Mask Detecting Using Artificial Intelligence, Machine Learning and Computer Vision',
+      imageUrl: '/assets/face-mask-detection.jpg',
+      buttonUrl: 'https://www.williamto.com/assets/files/face_mask_detection_research_question.pdf',
+      description: [
+        'In response to the COVID-19 pandemic, which has affected over 603 million people and caused nearly 7 million deaths, my capstone project investigates the implementation of face mask detection technology using artificial intelligence (AI) and machine learning (ML). This research addresses the critical role of AI in enhancing public health measures, particularly through the enforcement of mask-wearing policies in densely populated areas. By utilizing advanced object detection algorithms, such as Convolutional Neural Networks (CNN) and You Only Look Once (YOLO), the study aims to develop efficient systems capable of real-time, accurate face mask detection. The project explores both the technical challenges and societal barriers, such as privacy concerns and legal restrictions, that impact the deployment of these technologies. Ultimately, the research highlights the potential of AI and ML to support public health strategies against highly transmissible diseases.',
+      ],
+      stats: [
+        {
+          name: 'Global Impact',
+          value: '603 million cases',
+        },
+        {
+          name: 'Fatalities',
+          value: '7 million deaths',
+        },
+        {
+          name: 'Detection Accuracy',
+          value: 'up to 98%',
+        },
+        {
+          name: 'Processing Speed',
+          value: 'milliseconds',
+        },
+      ],
     },
   ],
 };
