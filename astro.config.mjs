bd61af876cd5a87e0f750676bd3fae138ba9bdd2
William To <williamto222@gmail.com>
2024-05-06 01:36:09 +1000

    Added sitemap and robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), compress()],
+    site: 'https://www.williamto.com',
+    integrations: [tailwind(), compress(),
+        sitemap({
+          changefreq: "weekly",
+          priority: 0.7,
+          lastmod: new Date(),
+          entryLimit: 10000,
+        }),
+        robotsTxt()],
 });
