6a2912eed0268977dc9e4862dcb6312de644fa1a
William To <williamto222@gmail.com>
2024-05-07 23:52:42 +1000

    Fix download links to open new tab

---
+++ src/data/old-data.ts
@@ -0,0 +1,68 @@
+export const data = {
+  experience: [
+    {
+      name: 'Picnet',
+      location: 'Crows Nest, NSW',
+      description:
+        "I am currently employed as an junior engineer at Picnet, an IT service and consulting firm whose clients include some of Australia's largest businesses and household names in the corporate and public sectors.",
+      startDate: 'Feb 2022',
+      endDate: 'Present',
+      projects: [
+        {
+          name: 'NSW Health Database Optimisation',
+          location: 'NSW E-Health',
+          descriptions: [
+            '•	Responsible for analysing data and creating optimised views for reporting complex data within NSW Health.',
+            '•	Improved the querying performance of previous reporting views and tables through indexing and caching data.',
+            '•	The key finding from analysing patient data were communicated in a business case report towards NSW Health senior management.',
+            '•	Attained knowledge of SQL Server Management Studio, Server Integration Services, Server Reporting Services, Server Analysis Services, and Server Data Tools.',
+          ],
+        },
+        {
+          name: 'NSW Health Web Application Security Enhancement',
+          location: 'NSW E-Health',
+          descriptions: [
+            '•	Designed and developed a more secure security architecture for the NSW Health Web Application using Active Directory, AngularJS and .Net framework.',
+            '•	Improved authentication performance for the NSW Health Web Application by about 55%.',
+            '•	Facilitated the migration of NSW Health Web Application from on-premises to cloud using Amazon Web Service.',
+            '•	Collaborated with NSW E-Health for the testing and deployment of the Web Application onto the new cloud platform.',
+            '•	Created documentation for the web application including database schemas diagrams for the purpose of documenting the database structure.',
+            '•	Reviewed code and developed unit test to ensure all security features are operational.',
+          ],
+        },
+        {
+          name: 'Centazio Data Integration Dashboard ',
+          location: 'PicNet',
+          descriptions: [
+            '•	Designed and develop the Centazio dashboard for monitoring the system using Sveltekit, IBM carbon design, Azure Web Service and .Net Framework.',
+            '•	Developed tools for monitoring and managing Azure Integration services using Azure API.',
+            '•	Developed code in accordance with appropriate design and architectural rules.',
+            '•	Working in a cross-functional team utilising agile development.',
+            '•	Deploying dashboard onto Azure Web Application.',
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Benevolence Financial Group',
+      location: 'Macquarie Park, NSW',
+      description:
+        'Benevolence Financial Group (BFG) is a social enterprise mortgage and finance brokerage firm which utilises technology to automate the process of applying for a loan.',
+      startDate: 'Aug 2020',
+      endDate: 'Apr 2022',
+      projects: [
+        {
+          name: 'BFG Home Loan CRM Web Application',
+          location: 'Benevolence Financial Group',
+          descriptions: [
+            '•	Built a proprietary home loan CRM tool using the ASP .NET Framework and sales trekker API for comparing and suggesting home loans to customers.',
+            '•	Supported end-to-end software development for CRM tools from concept to deployment. ',
+            '•	Developed landing pages for targeted marketing using HTML, CSS and boostrap.',
+            '•	Responsible for coordinating the team to ensure technical quality and on-time business delivery.',
+            '•	Acquired knowledge of Git command line and tortoise git, encompassing creating branches, commits, merging, pull requests, and pushing onto the repository.',
+          ],
+        },
+      ],
+    },
+  ],
+};
